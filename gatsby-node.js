5e29931def91f07f3bf7471ed92bf1a68a2cb30d
nbaugh1 <nbaugh1@gmail.com>
2020-02-03 22:04:11 -0500

    .

--- gatsby-node.js
+++ gatsby-node.js
@@ -5,3 +5,29 @@
  */
 
 // You can delete this file if you're not using it
+exports.createPages = async ({ actions, graphql }) => {
+    const results = await (graphql(`
+    {
+        allSanityPost {
+          edges {
+            node {
+              slug {
+                current
+              }
+            }
+          }
+        }
+      }
+    `));
+    
+    const posts = results.data.allSanityPost.edges.map(({ node }) => node);
+    posts.forEach(post => {
+        actions.createPage({
+            path: post.slug.current,
+            // template: 
+            context: {
+                slug: post.slug.current
+            }
+        })
+    })
+}
