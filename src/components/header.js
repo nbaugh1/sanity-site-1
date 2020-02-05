3c25e76b0383ed74346bddba76a1b2a5f72dfc5a
nbaugh1 <nbaugh1@gmail.com>
2020-02-04 21:43:04 -0500

    add links to post pages, colors for style theme

--- src/components/header.js
+++ src/components/header.js
@@ -5,7 +5,7 @@ import React from "react"
 const Header = ({ siteTitle }) => (
   <header
     style={{
-      background: `rebeccapurple`,
+      background: `black`,
       marginBottom: `1.45rem`,
     }}
   >
@@ -20,7 +20,7 @@ const Header = ({ siteTitle }) => (
         <Link
           to="/"
           style={{
-            color: `white`,
+            color: `#00EA61`,
             textDecoration: `none`,
           }}
         >
--- src/pages/index.js
+++ src/pages/index.js
@@ -33,8 +33,8 @@ const IndexPage = ({ data }) => (
     <h1>Portfolio</h1>
     <ul style={{ listStyle: 'none', display: 'flex', alignItems: 'space-between', padding: 0 }}>
       {data.allSanityPost.edges.map(({ node: post }) => (
-        <li key='{post.slug.current}' style={{ flex: '1 45%', maxWidth: '45%', margin: '1rem' }}>
-          <h2>{ post.title }</h2>
+        <li key={post.slug.current} style={{ flex: '1 45%', maxWidth: '45%', margin: '1rem' }}>
+      <Link to={post.slug.current}><h2>{ post.title }</h2></Link>
           <Image fluid={ post.mainImage.asset.fluid } ali={ post.title} />
         </li>
       
