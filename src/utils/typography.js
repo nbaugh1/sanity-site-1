091e0a7d555a047dfff008f6bfe7caedb417437a
nbaugh1 <nbaugh1@gmail.com>
2020-02-05 22:42:05 -0500

    change color of link hover to secondary color

--- src/utils/typography.js
+++ src/utils/typography.js
@@ -1,4 +1,14 @@
 import Typography from "typography";
 import judahTheme from 'typography-theme-judah'
+
+judahTheme.overrideThemeStyles = () => ({
+    "a:hover,a:active": {
+        color: '#6C7370',
+        textDecoration: "none",
+    },
+})
+
+
 const typography = new Typography(judahTheme);
+
 export default typography;
